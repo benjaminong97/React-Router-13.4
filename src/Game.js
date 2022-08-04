dc2376c960bb393edb71f914f3b217ead46f93d9
Benjamin Ong <89738593+benjaminong97@users.noreply.github.com>
2022-08-04 14:23:52 +0800

    Initial commit

---
+++ src/Game.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Game() {
+  return (
+    <React.Fragment>
+      <h1>THIS IS THE GAME!</h1>
+    </React.Fragment>
+  );
+}
---
+++ src/Splash.js
@@ -0,0 +1,9 @@
+import React from "react";
+export default function Splash() {
+  return (
+    <React.Fragment>
+      <h1>WELCOME!</h1>
+      <p>Redirecting in 5 seconds</p>
+    </React.Fragment>
+  );
+}
